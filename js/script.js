d913b6f8bd1c866f56202a6d0df61577b6797f14
tole9 <ante.tolusic9@gmail.com>
2017-09-13 15:28:21 +0200

    responsive for all devices, choose between C or F unit...

--- js/script.js
+++ js/script.js
@@ -28,14 +28,33 @@ window.onload = function() {
               if(request.readyState === 4 && request.status === 200){
                 var data = JSON.parse(request.responseText);
                 var currentTime = new Date();
-                console.log(data);
-                document.querySelector("#showTime").innerHTML = 'Today ' + currentTime.toLocaleString();
+                document.querySelector("#showTime").innerHTML = currentTime.toLocaleString();
                 cityName.innerHTML = '<i style="color: white;" class="fa fa-map-marker" aria-hidden="true"></i> ' + data.name + ', ' + data.sys.country;
                 showTemp.innerHTML = (data.main.temp - 273.15);
-                document.querySelector("#highLow").innerHTML = 'High: ' + (data.main.temp_min - 273.15) + '/Low: ' + (data.main.temp_max - 273.15);
+                document.querySelector("#highLow").innerHTML = '<i class="wi wi-thermometer"></i> ' + 'High: ' + (data.main.temp_min - 273.15) + '/Low: ' + (data.main.temp_max - 273.15);
+                tempUnit.addEventListener("click", function() {
+                  var tempUnit = document.querySelector("#tempUnit");
+                  if(tempUnit.textContent === '°C'){
+                    tempUnit.textContent = '°F';
+                    showTemp.innerHTML = (data.main.temp_min - 273.15) * 9/5 + 32;
+                    showTemp.classList.add('edit');
+                    document.querySelector("#showTime").style.marginTop = '20px';
+                    document.querySelector("#highLow").innerHTML = '<i class="wi wi-thermometer"></i> ' + 'High: ' + (data.main.temp_min - 273.15) * 9/5 + 32 + '/Low: ' + (data.main.temp_max - 273.15) * 9/5 + 32;
+                  } else {
+                    tempUnit.textContent = '°C';
+                    showTemp.classList.remove('edit');
+                    document.querySelector("#showTime").style.marginTop = '0px';
+                    showTemp.innerHTML = (data.main.temp - 273.15);
+                    document.querySelector("#highLow").innerHTML = '<i class="wi wi-thermometer"></i> ' + 'High: ' + (data.main.temp_min - 273.15) + '/Low: ' + (data.main.temp_max - 273.15);
+                  }
+                });
                 document.querySelector("#humidity").innerHTML = data.main.humidity + '%';
                 document.querySelector("#pressure").innerHTML = data.main.pressure + ' hPa';
-                document.querySelector("#wind").innerHTML = 'S: ' + data.wind.speed + ' km/h,' + '<br>' + 'D: ' + data.wind.deg + '°';
+                if(data.wind.deg === undefined){
+                  document.querySelector("#wind").innerHTML = 'S: ' + data.wind.speed + ' km/h'
+                } else{
+                  document.querySelector("#wind").innerHTML = 'S: ' + data.wind.speed + ' km/h,' + '<br>' + 'D: ' + data.wind.deg + '°';
+                }
                 iconGen(data.weather[0].icon);
               }
             }
@@ -45,4 +64,5 @@ window.onload = function() {
         var srcAttr = 'icons/' + vrijeme + '.png';
         document.querySelector('.weatherImage').setAttribute('src', srcAttr);
         }
+
 }
